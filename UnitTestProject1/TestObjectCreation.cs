dd9835c261cc4102b32a2a7524731eee234fb305
TimBrouwers <tbr@live.nl>
2016-03-22 10:45:35 +0100

    hoi

---
+++ UnitTestProject1/TestObjectCreation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalShelter;
+
+namespace AnimalShelterTest
+{
+    [TestClass]
+    public class TestObjectCreation
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
