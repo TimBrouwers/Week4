e4bca0d13bf82584d145dade92e9d45790f953bf
Mindavi <rol3517@gmail.com>
2016-04-11 22:56:08 +0200

    consistency for the asserts

--- UnitTestProject1/AdministrationTests.cs
+++ UnitTestProject1/AdministrationTests.cs
@@ -133,7 +133,7 @@ namespace AnimalShelter.Tests
             Assert.IsFalse(administration.Animals.Contains(testAnimal));
             Assert.IsFalse(administration.Animals.Contains(testAnimal2));
             administration.Load(pathToTestFile);
-            Assert.IsTrue(administration.FindAnimal(testAnimal.ChipRegistrationNumber).ToString() == testAnimal.ToString());
+            Assert.AreEqual(administration.FindAnimal(testAnimal.ChipRegistrationNumber).ToString(), testAnimal.ToString());
             Assert.AreEqual(administration.FindAnimal(testAnimal2.ChipRegistrationNumber).ToString(), testAnimal2.ToString());
             /* does not work
             CollectionAssert.Contains(administration.Animals, testAnimal);
