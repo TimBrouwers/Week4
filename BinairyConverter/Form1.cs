f0c0c2be3263ebc1af156dd94af19653959a908d
TimBrouwers <tbr@live.nl>
2016-04-11 17:32:52 +0200

    made buggy code for importing files exported from animalshelter

--- BinairyConverter/Form1.cs
+++ BinairyConverter/Form1.cs
@@ -20,18 +20,34 @@ namespace BinairyConverter
 
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
-            Stream mystream = null;
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "textfile(*.txt)|*.txt|All files(*.)|*.*";
+            byte[] buffer = new byte[256];
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    if ((mystream = ofd.OpenFile()) != null)
+                    using (var mystream = File.Open(ofd.FileName, FileMode.Open))
                     {
-                        using (mystream)
+                        for (int i = 0; i < mystream.Length + 1; i = i + 256)
                         {
-                            
+                            mystream.Read(buffer, i, 256);
+                            var sbuffer = System.Text.Encoding.Default.GetString(buffer);
+                            Console.WriteLine(sbuffer.IndexOf("Cat: "));
+                            if (sbuffer.IndexOf("Dog: ") > -1)
+                            {
+                                string chipNumber = sbuffer.Substring(sbuffer.IndexOf(":") + 1, sbuffer.IndexOf(":") - sbuffer.IndexOf(","));
+                                Console.WriteLine(chipNumber);
+                                //string dogInfo = sbuffer.Substring(sbuffer.IndexOf(":") + 1, sbuffer.IndexOf(":") - sbuffer.IndexOf(","));
+                            }
+                            else if (sbuffer.IndexOf("Cat: ") > -1)
+                            {
+
+                            }
+                            else
+                            {
+                                //throw new InvalidDataException("File is invalid");
+                            }
                         }
                     }
                 }
